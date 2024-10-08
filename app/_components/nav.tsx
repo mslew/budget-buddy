253c6acf786781f81fcfc19976a24698b4e7028a
mslew <mlew3223@gmail.com>
2024-10-07 22:20:15 -0500

    hide API Key

---
+++ app/_components/nav.tsx
@@ -0,0 +1,5 @@
+export default function Nav(){
+    return (
+        <div className="w-screen h-20 bg-red-400"></div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Nav from "./_components/nav";
 import "./globals.css";
 
 const inter = Inter({
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body
         className={inter.className}
       >
+        <Nav />
         {children}
       </body>
     </html>
