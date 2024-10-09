136166f88cb89cf036e58e6c33bb025ad23807c1
mslew <mlew3223@gmail.com>
2024-10-09 13:10:48 -0500

    folders for different pages and hero starting

--- app/_components/nav.tsx
+++ app/_components/nav.tsx
@@ -1,24 +1,26 @@
-import NavLink from "./navlink"
-import Image from "next/image"
+import NavLink from "./navlink";
+import Image from "next/image";
 
-export default function Nav(){
-    return (
-        <nav className="relative flex flex-row justify-center items-center text-white pt-4">
-            <div className="w-full lg:w-2/3 h-[90%] flex flex-row justify-between items-center pl-4 pr-4">
-                <div className="h-3/4 w-1/4 flex flex-row flex-shrink justify-center items-center p-3">
-                    <Image
-                        src={"/logos/logo_light.png"}
-                        height={100}
-                        width={100}
-                        alt="logo"
-                    />
-                </div>
-                <div className="h-3/4 w-1/2 flex flex-row-reverse flex-shrink gap-2 space-x-1 items-center">
-                    <button className="border rounded-lg p-3">Create</button>
-                    <NavLink href="/" text="Login" />
-                    <NavLink href="/" text="Register" />
-                </div>
-            </div>
-        </nav>
-    )
+export default function Nav() {
+  return (
+    <nav className="relative flex flex-row justify-center items-center text-white pt-4">
+      <div className="w-full lg:w-2/3 h-[90%] flex flex-row justify-between items-center pl-4 pr-4">
+        <div className="h-3/4 w-1/4 flex flex-row flex-shrink justify-center items-center p-3">
+          <a className="block" href="/">
+            <Image
+                src={"/logos/logo_light.png"}
+                height={100}
+                width={100}
+                alt="logo"
+            />
+          </a>
+        </div>
+        <div className="h-3/4 w-1/2 flex flex-row-reverse flex-shrink gap-2 space-x-1 items-center">
+          <button className="border rounded-lg p-3">Create</button>
+          <NavLink href="/login" text="Login" />
+          <NavLink href="/register" text="Register" />
+        </div>
+      </div>
+    </nav>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import Hero from "./_components/hero";
 
 export default function Home() {
   return (
-    <></>
+    <Hero />
   );
 }
