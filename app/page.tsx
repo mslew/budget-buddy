c680229543afc2d2f2af89a43431d44324b5197a
mslew <mlew3223@gmail.com>
2024-10-09 00:11:24 -0500

    picture for stephen

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <Image
+        src={"/images/miku_and_chief.png"}
+        width={500}
+        height={500}
+        alt="chief and miku"
+      />
+    </>
   );
 }
