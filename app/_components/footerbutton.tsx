974006d820d0515cd5d0a11425b4b2b571ce4425
mslew <mlew3223@gmail.com>
2024-10-10 14:12:02 -0500

    more updates

--- app/_components/footerbutton.tsx
+++ app/_components/footerbutton.tsx
@@ -8,17 +8,17 @@ interface Props{
 
 export default function FooterButton({bgColor, logoPath, href, children}: Props){
     return(
-        <a href={href} target="_blank" className={`${bgColor} flex flex-row gap-4 rounded-lg h-min justify-center items-center border border-black text-white text-sm`}>
+        <a href={href} target="_blank" className={`${bgColor} flex flex-row flex-shrink-0 gap-4 rounded-lg h-min justify-center items-center border border-black text-white text-sm`}>
             <div className="flex flex-row items-center gap-2">
                 <div className="border-r p-2">
                     <Image 
                         src={logoPath}
-                        height={"30"}
-                        width={"30"}
+                        height={"20"}
+                        width={"20"}
                         alt={"logo"}
                     />
                 </div>
-                <p className="w-min whitespace-nowrap p-2">{children}</p>
+                <p className="w-min whitespace-nowrap p-2 text-sm">{children}</p>
             </div>
         </a>
     )   
--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -1,13 +1,13 @@
 export default function Hero(){
     return(
         <div className="w-full h-[80%] flex flex-col items-center pt-20">
-            <div className="w-3/4 h-1/2 text-center text-white flex flex-col">
-                <div className="text-7xl">
+            <div className="w-3/4 h-1/2 text-center text-white flex flex-col items-center gap-10">
+                <div className="lg:text-7xl text-3xl font-bold">
                     <p>Welcome to Budget Buddy!</p>
-                    <p>Your one stop shop for all your budgeting needs.</p>
+                    <p>The best place for you to create a budget!</p>
                 </div>
+                <button className="p-4 border text-white rounded-3xl w-min whitespace-nowrap text-4xl bg-slate-500">Create a Budget!</button>
             </div>
-            <button className="p-2 border text-white rounded-lg">Create a Budget!</button>
         </div>
     )
 }
