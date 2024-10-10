58cbad7968dac41b4122a1f4c980672a3ab0079d
mslew <mlew3223@gmail.com>
2024-10-10 15:08:36 -0500

    button stuff

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -2,7 +2,7 @@ import FooterButton from "./footerbutton"
 
 export default function Footer(){
     return(
-        <footer className="w-full h-[10%] flex flex-row items-center justify-center pb-4">
+        <footer className="w-full h-[10%] flex flex-row items-center justify-center pb-4 fixed bottom-0">
             <div className="h-min flex flex-row flex-wrap justify-center gap-2">
                 <FooterButton href="https://vercel.com" logoPath="/logos/vercel.png" bgColor="bg-black">Powered By Vercel</FooterButton>
                 <FooterButton href="https://firebase.google.com" logoPath="/logos/firebase.png" bgColor="bg-orange-500">Powered By Firebase</FooterButton>
--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -2,13 +2,13 @@ import CreateButton from "./createbutton";
 
 export default function Hero() {
   return (
-    <div className="w-full h-[80%] flex flex-col items-center pt-20">
+    <div className="w-full h-[80%] flex flex-col items-center justify-center">
       <div className="w-full md:w-3/4 h-1/2 text-center text-white flex flex-col items-center gap-10">
         <div className="lg:text-7xl text-5xl font-bold">
           <p>Welcome to Budget Buddy!</p>
           <p>The best place for you to create a budget!</p>
         </div>
-        <CreateButton textSize="lg:text-4xl text-2xl gap-4" height="lg:h-10 h-6">
+        <CreateButton textSize="lg:text-4xl text-2xl gap-3" height="lg:h-10 h-6">
           Create a Budget
         </CreateButton>
       </div>
